6f7cadd40045275181223e7bbb722ea753de9b06
emmanuel roman <emmaaanu9@gmail.com>
2023-05-25 23:57:57 -0300

    actualizado

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.querySelector(".container-fluid");
-  const menu = document.querySelector(".menu");
-
-  menuToggle.addEventListener("click", function () {
-    menu.classList.toggle("open");
-  });
-});
