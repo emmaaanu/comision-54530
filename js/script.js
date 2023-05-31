4c15e8b4172169d8359469a48ed59a59d0e5ebc7
emmanuel roman <emmaaanu9@gmail.com>
2023-05-31 00:14:19 -0300

    responsi 0.1

--- js/script.js
+++ js/script.js
@@ -2,9 +2,33 @@ const toggleBtn = document.querySelector(".toggle_btn");
 const toggleBtnIcon = document.querySelector(".toggle_btn i");
 const dropDownMenu = document.querySelector(".dropdown_menu");
 
-toggleBtn.onclick = function () {
-  dropDownMenu.classList.toggle("open");
-  const isOpen = dropDownMenu.classList.contains("open");
-
-  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+// Verificar el estado almacenado en localStorage al cargar la página
+window.onload = function () {
+  const isOpen = localStorage.getItem("menuOpen") === "true";
+  dropDownMenu.classList.toggle("open", isOpen);
+  toggleBtnIcon.classList.toggle("fa-xmark", isOpen);
 };
+
+// Función para guardar el estado del menú en localStorage
+function saveMenuState(isOpen) {
+  localStorage.setItem("menuOpen", isOpen);
+}
+
+toggleBtn.onclick = function (event) {
+  event.stopPropagation(); // Evitar que el evento se propague a los enlaces
+
+  const isOpen = dropDownMenu.classList.toggle("open");
+  toggleBtnIcon.classList.toggle("fa-xmark", isOpen);
+
+  saveMenuState(isOpen);
+};
+
+// Cerrar el menú cuando se hace clic en un enlace
+const links = document.querySelectorAll(".dropdown_menu a");
+links.forEach(function (link) {
+  link.onclick = function () {
+    dropDownMenu.classList.remove("open");
+    toggleBtnIcon.classList.remove("fa-xmark");
+    saveMenuState(false);
+  };
+});
